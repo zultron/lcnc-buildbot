2bc6288acbd1cb44a9e46d68c566f924390dd7fa
root <root@bb0.cf0.zultron.com>
2013-11-17 17:47:12 -0600

    Mammoth, unhygeinic commit that touches everything, yuck
    I didn't follow Linus's advice, 'commit early, commit often'.  Sorry.
    
    Move change sources into config file:
    - currently only supports git poller
    
    Move factory definitions into config file:
    - Pull shell code out into buildsteps.sh:
      - shell script called as 'buildsteps.sh <step>'
      - contains Kent's original steps, less the initial git steps
      - if config file build factory has the optional 'buildsteps-script'
        attribute, copy that to build slaves in first step
    - Move build steps into config file:
      - 'source.git' type step
      - 'script' (default) type runs buildsteps script with step name as
        argument
      - 'script.tty' type does the same, but with a tty
    
    Schedulers:
    - FIXME this is a hack until we have a more complex example to model
    - For now, each change source gets two hard-wired schedulers, 'all'
      and 'force'
    - Each scheduler gets all builders
    - Both of the above may need to be configurable from config file,
      we'll see
    
    Odds and ends:
    - remove obsolete 'repo' configuration dict from master.cfg
    - update config.yaml.sample examples and comments
    - add debugging flag and print stuff when set

---
+++ buildsteps.sh
@@ -0,0 +1,101 @@
+#!/bin/bash -xe
+
+# Where the clean git repo is checked out into
+#
+# Must match the 'workdir' setting in config.yaml (which makes this
+# liable to break :P )
+REPODIR=repo
+
+# This script starts execution in 'workdir'
+WORKDIR="$(pwd)"
+
+# At least a step should be defined
+usage="usage:  $0 <step>"
+step=${1:?$usage}; shift
+
+# Fix relative paths
+if test $REPODIR = ${REPODIR#/}; then   # relative path
+    # Always assume we're starting in the 'build' directory
+    # FIXME This is a terrible assumption.  How to pass in these params?
+    REPODIR="$(readlink -f $(pwd)/../$REPODIR)"
+fi
+
+step-sourcetree() {
+    cd $REPODIR
+    git archive --prefix=source/ HEAD | tar xCf "$WORKDIR" -
+}
+
+step-dmesg() {
+    sudo dmesg -c
+}
+
+step-environment() {
+    echo 'uname -a:'; 
+    uname -a; 
+    echo; 
+    echo 'ulimit -a:'; 
+    ulimit -a; 
+    echo; 
+    echo 'git --version:'; 
+    git --version; 
+    echo; 
+    echo 'gcc --version:'; 
+    gcc --version; 
+    echo; 
+    echo 'python -V:'; 
+    python -V; 
+    echo; 
+    if test -x /usr/bin/lsb_release; then
+	echo 'lsb_release --all:'; 
+	lsb_release --all; 
+	echo; 
+    else
+	    # what does this do, and how to replicate on RH?
+	    # (RH has lsb, but the package drags in a whole X environment)
+	: # do nothing
+    fi
+    echo 'lsmod:'; 
+    lsmod; 
+    echo; 
+    if test -x /bin/rpm; then
+	echo "rpm -qa:"
+	rpm -qa
+    elif test -x /usr/bin/dpkg; then
+	    # Ubuntu
+	echo 'dpkg --get-selections:'; 
+	dpkg --get-selections;
+    fi
+}
+
+step-autogen() {
+    cd source/src
+    ./autogen.sh
+}
+
+step-configure() {
+    cd source/src
+    ./configure
+}
+
+step-make() {
+    cd source/src
+    make V=1
+}
+
+step-setuid() {
+    cd source/src
+    sudo make setuid
+}
+
+step-runtests() {
+    cd source
+    source ./scripts/rip-environment
+    runtests -v
+}
+
+step-closeout() {
+    dmesg
+}
+
+step-$step || { echo "$usage" 1>&2; exit 1; }
+
